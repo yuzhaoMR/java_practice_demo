9bcbbd5ae777da4a6279b77be66a0cc2007fad37
yuzhaoMR <444894151@qq.com>
2020-08-29 17:10:37 +0800

    init project

---
+++ 01_guess_number/src/main/java/com/demo/guess_number/JavaSpringbootScaffoldingApplication.java
@@ -0,0 +1,12 @@
+package com.demo.guess_number;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootScaffoldingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootScaffoldingApplication.class, args);
+    }
+}
---
+++ 01_guess_number/src/test/java/com/demo/guess_number/JavaSpringbootScaffoldingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.demo.guess_number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JavaSpringbootScaffoldingApplicationTests {
+
+    @Test
+    void test() {
+        assertEquals(true, true);
+    }
+}
